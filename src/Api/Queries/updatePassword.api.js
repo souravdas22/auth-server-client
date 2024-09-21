976a211f9965f087ad73ae1cda4518e49cead2ce
Coolsourav <9163905324das@gmail.com>
2024-09-21 10:39:23 +0530

    update pass bug fixed

--- src/Api/Queries/updatePassword.api.js
+++ src/Api/Queries/updatePassword.api.js
@@ -4,7 +4,7 @@ import axiosInstance from "../../helper/axiosInstance";
 export const updatePassword = async ({ data,token }) => {
     try {
       console.log(data,token)
-    const response = await axiosInstance.post(`/update-password/${token}`, data);
+    const response = await axiosInstance.post(`/update-password`, data);
     return response?.data;
   } catch (error) {
     toast.error(error?.response?.data?.message);
